45985eff26b4ded82208458887147ebdc7f5ede3
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-09-11 09:06:43 -0400

    update-automated-save

--- mail/creds/credentials.go
+++ mail/creds/credentials.go
@@ -53,64 +53,66 @@ type CREDS struct {
 	b      []byte
 	file   string
 	dir    string
-	client *http.Client
+	client []*http.Client
 	token  []*oauth2.Token
-	srv    *gmail.Service
+	srv    []*gmail.Service
 }
 
-func NewGmailSrv() *gmail.Service {
+func NewGmailSrv() ([]*gmail.Service, error) {
 	c := CREDS{}
-	c.PopulateCREDS()
-	srv := c.GetSRV()
-	return srv
+	err := c.PopulateCREDS()
+	if err != nil {
+		return []*gmail.Service{}, err
+	}
+	return c.GetSRV()
+
 }
 
-func ListTokens(root string) ([]string, error) {
+func ListTokenFiles(root string) ([]string, error) {
 	files := []string{}
 	token_files := []string{}
-
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		files = append(files, path)
 		return nil
 	})
 	if err != nil {
-		return files,err
+		return files, err
 	}
 	for _, file := range files {
-		if strings.Contains(file,"token") {
-			token_files = append(token_files,file)
+		if strings.Contains(file, "token") {
+			token_files = append(token_files, file)
 		}
 	}
 	sort.Strings(token_files)
 	return token_files, err
 }
 
-
-
-func (c *CREDS) PopulateCREDS() {
+func (c *CREDS) PopulateCREDS() error {
 	dir, err := FindDir()
 	if err != nil {
-		log.Fatalf("Can't find credential file")
+		return err
 	}
 	c.b = ReadCredentials(dir)
 
-	token, err := tokenFromFile(dir + "/token.json")
+	files, err := ListTokenFiles(dir)
 	if err != nil {
-		log.Printf("Can't read token.json. %v\n", err)
-		log.Printf("Error msg: %v\n", err)
-		token, err = tokenFromFile("/credentials/token.json")
-		if err != nil {
-			log.Printf("NOPE. NOT in /credentials/token.json")
-			log.Printf("Error msg: %v\n", err)
-			return
-		}
-
-		log.Printf("GOT IT.  /credentials/token.json")
+		return err
 	}
-	c.token[0] = token
-}
 
+	for _, file := range files {
+
+		token, err := tokenFromFile(file)
+		if err != nil {
+			log.Printf("Can't read token.json. %v\n", err)
+			log.Printf("Error msg: %v\n", err)
+			return err
+
+		}
+		c.token = append(c.token,token)
+	}
+	return nil
+}
 
 func (c *CREDS) PopulateCREDS2() {
 	dir, err := FindDir()
@@ -135,7 +137,6 @@ func (c *CREDS) PopulateCREDS2() {
 	c.token[0] = token
 }
 
-
 func ReadCredentials(dir string) []byte {
 	b, err := ioutil.ReadFile(dir + "/credentials.json")
 	if err != nil {
@@ -155,19 +156,24 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-func (c *CREDS) GetSRV() *gmail.Service {
+func (c *CREDS) GetSRV() ([]*gmail.Service, error) {
 
 	config, err := google.ConfigFromJSON(c.b, gmail.MailGoogleComScope)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		log.Printf("GetSRV()")
+		return []*gmail.Service{}, err
 	}
 
-	c.client = config.Client(context.Background(), c.token[0])
+	for i, token := range c.token {
 
-	srv, err := gmail.New(c.client)
+
+	c.client = append(c.client,config.Client(context.Background(), token))
+
+	srv, err := gmail.New(c.client[i])
 	if err != nil {
 		log.Fatalf("Unable to retrieve Gmail client: %v", err)
 	}
-	c.srv = srv
-	return srv
+	c.srv = append(c.srv,srv)
+   }
+	return c.srv,err
 }
--- mail/creds/credentials_test.go
+++ mail/creds/credentials_test.go
@@ -10,22 +10,26 @@ import (
 
 func TestListTokens(t *testing.T) {
 	dir, _ := FindDir()
-	r,_ :=ListTokens(dir)
-	fmt.Printf("%v\n",r)
+	r, _ := ListTokenFiles(dir)
+	fmt.Printf("%v\n", r)
 }
 
 func Test_findDir(t *testing.T) {
 
 	c := CREDS{}
 	c.PopulateCREDS()
+	if len(c.token) == 0 {
+		t.Fatal()
+	}
 
 }
 
 func Test_GetSRV(t *testing.T) {
 
-	srv := NewGmailSrv()
+	srv, _ := NewGmailSrv()
+
 	user := "me"
-	r, err := srv.Users.Labels.List(user).Do()
+	r, err := srv[0].Users.Labels.List(user).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve labels: %v", err)
 	}
@@ -37,9 +41,9 @@ func Test_GetSRV(t *testing.T) {
 	for _, l := range r.Labels {
 		fmt.Printf("- %s\n", l.Name)
 	}
-	msg, _ := srv.Users.Messages.List("me").LabelIds("TRASH").Do()
+	msg, _ := srv[0].Users.Messages.List("me").LabelIds("TRASH").Do()
 
-	nsrv := gmail.NewUsersService(srv)
+	nsrv := gmail.NewUsersService(srv[0])
 	nsrv.Messages.List("me").LabelIds("TRASH").Do()
 
 	//msg, _ := srv.Users.Messages.List("me").Do()
@@ -51,7 +55,7 @@ func Test_GetSRV(t *testing.T) {
 		if count > 9 {
 			continue
 		}
-		g, _ := srv.Users.Messages.Get("me", v.Id).Format("raw").Do()
+		g, _ := srv[0].Users.Messages.Get("me", v.Id).Format("raw").Do()
 		fmt.Println(g.Snippet)
 		data, _ := base64.StdEncoding.DecodeString(g.Raw)
 		sdata := fmt.Sprintf("%s", data)
